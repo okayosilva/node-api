a8cd73f265d7a9cd64d067b8227b2e14c353b48e
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-11 16:34:17 -0300

    add controllers

---
+++ src/controllers/UsersControllers.js
@@ -0,0 +1,11 @@
+class UsersControllers {
+  /**
+   * index - GET para listar vários registros.
+   * show - GET para exibir um registro específico.
+   * create - POST para criar um registro.
+   * update - PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
+}
+
+module.exports = new UsersControllers
