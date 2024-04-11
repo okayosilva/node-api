cfa9b0d3923e049b9b09bee069f9ef513effb70d
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-11 17:16:21 -0300

    add middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,23 @@ const UsersController = require('../controllers/UsersController')
 
 const usersRoutes = Router()
 
+
+
+function myMiddleware(request, response, next) {
+  const { isAdmin } = request.body
+  
+  if (!isAdmin) {
+    return response.status(401).json({ error: 'Unauthorized' })
+  }
+
+  console.log(request.body)
+  return next()
+}
+
+
+
 const usersController = new UsersController()
 
-usersRoutes.post('/', usersController.create)
+usersRoutes.post('/', myMiddleware, usersController.create)
 
 module.exports = usersRoutes
