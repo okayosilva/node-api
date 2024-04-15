e40769fbf07bdcf775dcce4944a0f3357c0416fc
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-15 20:12:28 -0300

    :sparkles: migrations

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
 
@@ -29,6 +29,55 @@ class UsersController {
 
     return response.status(201).json()
     
+  }
+
+  async update (request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user) {
+      throw new AppError('User not found', 404)
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('The user s email is already in use', 400)
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(password && !old_password) {
+      throw new AppError('You need to enter your old password to be able to set a new password.')
+    }
+
+    if(password && old_password) {
+      const chekOldPassword = await compare(old_password, user.password)
+      
+      if(!chekOldPassword) {
+        throw new AppError('Your old password is incorrect', 400)
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,  
+      [user.name , user.email, user.password, id]
+    )
+
+
+    return response.status(200).json()
+
   }
 }
 
