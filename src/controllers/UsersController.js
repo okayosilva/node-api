dd7dbb956d9869e8fcf8cf83f98d5ab8d975ae05
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-15 15:18:44 -0300

    feat: migrations / add user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,6 @@
+const { hash } = require('bcryptjs')
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
   /**
@@ -9,14 +11,24 @@ class UsersController {
    * delete - DELETE para remover um registro.
    */
 
-  create(request, response) {
+ async create(request, response) {
     const { name, email, password } = request.body
-    
-    if(!name) {
-      throw new AppError('Name is required', 400)
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(checkUserExists) {
+      throw new AppError('User email already exists', 400)
     }
 
-    response.status(201).json({ name, email, password })
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+       [name, email, hashedPassword]
+    )
+
+    return response.status(201).json()
+    
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,22 +5,20 @@ const UsersController = require('../controllers/UsersController')
 const usersRoutes = Router()
 
 
-
-function myMiddleware(request, response, next) {
-  const { isAdmin } = request.body
+// function myMiddleware(request, response, next) {
+//   const { isAdmin } = request.body
   
-  if (!isAdmin) {
-    return response.status(401).json({ error: 'Unauthorized' })
-  }
+//   if (!isAdmin) {
+//     return response.status(401).json({ error: 'Unauthorized' })
+//   }
 
-  console.log(request.body)
-  return next()
-}
-
+//   console.log(request.body)
+//   return next()
+// }
 
 
 const usersController = new UsersController()
 
-usersRoutes.post('/', myMiddleware, usersController.create)
+usersRoutes.post('/', usersController.create)
 
 module.exports = usersRoutes
