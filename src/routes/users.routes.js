f48fc1b4a98749c96eef8a464eff420df3cb66f5
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-16 13:44:25 -0300

    :sparkles: feat: Notes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,19 +4,6 @@ const UsersController = require('../controllers/UsersController')
 
 const usersRoutes = Router()
 
-
-// function myMiddleware(request, response, next) {
-//   const { isAdmin } = request.body
-  
-//   if (!isAdmin) {
-//     return response.status(401).json({ error: 'Unauthorized' })
-//   }
-
-//   console.log(request.body)
-//   return next()
-// }
-
-
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
