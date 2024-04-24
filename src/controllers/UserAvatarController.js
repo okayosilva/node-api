473ee0b83f55b822115a4be29711ca9d0aa352dc
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-24 07:56:18 -0300

    Refactor UserAvatarController to use DiskStorage provider for avatar upload

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -1,12 +1,33 @@
-
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const DiskStorage = require("../providers/DiskStorage")
 
 class UserAvatarController {
   async update(request, response) {
     const user_id = request.user.id
     const avatarFileName = request.file.filename
+
+    const diskStorage = new DiskStorage()
  
+    const user = await knex("users")
+      .where({ id: user_id }).first()
 
+    if(!user) {
+      throw new AppError('Somente usuários autenticados podem mudar o avatar', 401)
+    }
 
+    if(user.avatar) {
+      await diskStorage.delete(user.avatar)
+    }
+
+    const fileName = await diskStorage.save(avatarFileName)
+    user.avatar = fileName
+
+    await knex("users")
+      .where({ id: user_id })
+      .update(user)
+
+      return response.json(user)
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,18 +4,17 @@ const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/UsersController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+const UserAvatarController = require('../controllers/UserAvatarController')
 
 const usersRoutes = Router()
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 const upload = multer(uploadConfig.MULTER)
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (req, res) => {
-  console.log(req.file.filename)
-  res.status(200).send()
-} )
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 module.exports = usersRoutes
